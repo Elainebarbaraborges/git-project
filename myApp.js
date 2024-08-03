28d5651fd91117a710394ae82a50ca7584e5393b
ElaineBorges <elaineb.borges@hotmail.com>
2024-08-03 15:08:06 +0100

    Added myApp.js file

---
+++ myApp.js
@@ -0,0 +1,11 @@
+// 2024 July Git Course
+
+const myApp = {
+    name: "My App",
+    version: "1.0.0",
+    init: function (){
+        console.log(`${this.name} ${this.version}`);
+    },
+};
+
+myApp.init();
